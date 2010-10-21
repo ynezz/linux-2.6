90b770c8503ec1ab59c918c85e7ba8fe1abeec82
Petr Štetiar <ynezz@true.cz>
2010-10-21 20:00:49 +0200

    ARM: ep93xx: use more reliable CPLD watchdog for reset on ts72xx
    On all ep93xx based boards from Technologic Systems, there's CPLD watchdog
    available, so use this one to reset the board instead of the soft reset in
    CPU.  I've seen some weird lockups with the soft reset on ep93xx in the past,
    while the reset via CPLD watchdog seems to be rock solid (tm) and works fine
    so far.
    
    Cc: Ryan Mallon <ryan@bluewatersys.com>
    Acked-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Tested-by: Mika Westerberg <mika.westerberg@iki.fi>
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- arch/arm/mach-ep93xx/include/mach/system.h
+++ arch/arm/mach-ep93xx/include/mach/system.h
@@ -2,7 +2,10 @@
  * arch/arm/mach-ep93xx/include/mach/system.h
  */
 
+#include <linux/io.h>
+
 #include <mach/hardware.h>
+#include <mach/ts72xx.h>
 
 static inline void arch_idle(void)
 {
@@ -13,11 +16,16 @@ static inline void arch_reset(char mode, const char *cmd)
 {
 	local_irq_disable();
 
-	/*
-	 * Set then clear the SWRST bit to initiate a software reset
-	 */
-	ep93xx_devcfg_set_bits(EP93XX_SYSCON_DEVCFG_SWRST);
-	ep93xx_devcfg_clear_bits(EP93XX_SYSCON_DEVCFG_SWRST);
+	if (board_is_ts7200() || board_is_ts7250() || board_is_ts7260() ||
+	    board_is_ts7300() || board_is_ts7400()) {
+		/* We use more reliable CPLD watchdog to perform the reset */
+		__raw_writeb(0x5, TS72XX_WDT_FEED_PHYS_BASE);
+		__raw_writeb(0x1, TS72XX_WDT_CONTROL_PHYS_BASE);
+	} else {
+		/* Set then clear the SWRST bit to initiate a software reset */
+		ep93xx_devcfg_set_bits(EP93XX_SYSCON_DEVCFG_SWRST);
+		ep93xx_devcfg_clear_bits(EP93XX_SYSCON_DEVCFG_SWRST);
+	}
 
 	while (1)
 		;
