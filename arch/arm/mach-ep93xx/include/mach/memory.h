39fdca098e3e28bb619901637848898821e719c8
Petr Štetiar <ynezz@true.cz>
2011-05-29 16:52:30 +0200

    ARM: ep93xx: add partial sparsemem support
    Some boards, like for example ts-7300 with 128MB RAM as it seems can't be used
    with flat memory model correctly. This patch adds possibility to choose
    between flat/sparse memory models in kernel config and adds missing sparse
    memory bits for ep93xx.
    
    Patch is based on the previous discussion and code:
    
    	http://marc.info/?l=linux-arm&m=122754446724900&w=2
    
    I've tested this patch on the boards with 32MB and 128MB RAM.
    
    [LOCAL, not for mainline!]
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- arch/arm/mach-ep93xx/include/mach/memory.h
+++ arch/arm/mach-ep93xx/include/mach/memory.h
@@ -19,4 +19,31 @@
 #error "Kconfig bug: No EP93xx PHYS_OFFSET set"
 #endif
 
+/*
+ * Non-linear mapping like so:
+ * phys       => virt
+ * 0x00000000 => 0xc0000000
+ * 0x01000000 => 0xc1000000
+ * 0x04000000 => 0xc4000000
+ * 0x05000000 => 0xc5000000
+ * 0xe0000000 => 0xc8000000
+ * 0xe1000000 => 0xc9000000
+ * 0xe4000000 => 0xcc000000
+ * 0xe5000000 => 0xcd000000
+ *
+ * As suggested here: http://marc.info/?l=linux-arm&m=122754446724900&w=2
+ */
+#if defined(CONFIG_SPARSEMEM)
+
+#define __phys_to_virt(p) \
+	(((p) & 0x07ffffff) | (((p) & 0xe0000000) ? 0x08000000 : 0) | PAGE_OFFSET)
+
+#define __virt_to_phys(v) \
+	(((v) & 0x07ffffff) | (((v) & 0x08000000) ? 0xe0000000 : 0 ))
+
+#define SECTION_SIZE_BITS 24
+#define MAX_PHYSMEM_BITS 32
+
+#endif /* CONFIG_SPARSEMEM */
+
 #endif
