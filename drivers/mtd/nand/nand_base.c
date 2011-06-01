454921bb8fe5d7fb3b76bcf47d42ed0956495f20
Jiri Pinkava <jiri.pinkava@vscht.cz>
2011-06-01 15:56:40 +0200

    mtd: nand: remove meaningless delay from nand_unlock
    This delay is meaningless. If delay is needed it is device specific
    and must be reimplemented by specific driver, otherwise no delay is
    needed.
    
    Signed-off-by: Jiri Pinkava <jiri.pinkava@vscht.cz>
    Acked-by: Vimal Singh <vimal.newwork@gmail.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -919,7 +919,6 @@ static int __nand_unlock(struct mtd_info *mtd, loff_t ofs,
 
 	/* Call wait ready function */
 	status = chip->waitfunc(mtd, chip);
-	udelay(1000);
 	/* See if device thinks it succeeded */
 	if (status & 0x01) {
 		DEBUG(MTD_DEBUG_LEVEL0, "%s: Error status = 0x%08x\n",
@@ -1028,7 +1027,6 @@ int nand_lock(struct mtd_info *mtd, loff_t ofs, uint64_t len)
 
 	/* Call wait ready function */
 	status = chip->waitfunc(mtd, chip);
-	udelay(1000);
 	/* See if device thinks it succeeded */
 	if (status & 0x01) {
 		DEBUG(MTD_DEBUG_LEVEL0, "%s: Error status = 0x%08x\n",
